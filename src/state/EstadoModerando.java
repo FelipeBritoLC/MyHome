5982340402e10d1e2b917d620b8775f7ec6bb995
Felipe de Brito <felipebl1512@gmail.com>
2026-01-30 02:07:32 -0300

    Implementa padrão state - estados

---
+++ src/state/EstadoModerando.java
@@ -0,0 +1,16 @@
+package state;
+
+import builder.Anuncio;
+
+public class EstadoModerando implements EstadoAnuncio {
+    public void publicar(Anuncio contexto) {
+        System.out.println("Aprovação concluída! O anúncio agora está visível.");
+        contexto.setEstado(new EstadoAtivo());
+    }
+    public void cancelar(Anuncio contexto) {
+        System.out.println("Anúncio reprovado na moderação. Voltando para rascunho.");
+        contexto.setEstado(new EstadoRascunho());
+    }
+    public String getNomeEstado() { return "Em Moderação"; }
+}
+
